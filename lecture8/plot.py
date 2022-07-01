159ab9dc0f5adf8b79a3fb427121b0536edc748b
IumoInfinium <iumoinfinium1331@gmail.com>
2022-07-02 01:20:27 +0530

    +makefile ~plot.py

--- lecture8/plot.py
+++ lecture8/plot.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
 parser= argparse.ArgumentParser()
-parser.add_arguement('-i',type=argparse.FileType('r'))
-parser.add_arguement('-o')
-args = parser.parse_agrs()
+parser.add_argument('-i',type=argparse.FileType('r'))
+parser.add_argument('-o')
+args = parser.parse_args()
 
 data = np.loadtxt(args.i)
-plt.plot(data[:,0],data[:1])
+plt.plot(data[:,0],data[:,1])
 plt.savefig(args.o)
